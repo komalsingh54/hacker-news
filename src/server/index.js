6db712209cdf7aaec85ff77b2ebaa720765edcf2
Komal Singh <komalsingh54@gmail.com>
2020-04-11 02:10:13 +0000

    cors

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
 
 import handleRender from './handleRender';
 
@@ -11,9 +13,15 @@ const port = process.env.PORT || 54370;
 
 app.use(compression());
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/dist', express.static('dist'));
 app.use('/', express.static('public'));
+
+app.use('/', (req, res, next) => {
+  console.log(req.url);
+  next();
+});
 
 app.use(handleRender);
 

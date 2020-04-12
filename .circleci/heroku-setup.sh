297e9b06db695cba1c904f508c0913b0f02bd57d
Komal Singh <komalsingh54@gmail.com>
2020-04-12 23:26:41 +0530

    Heroku (#5)

---
+++ .circleci/heroku-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
+
+cat > ~/.netrc << EOF
+machine api.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
+machine git.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
+EOF
+
+echo $CIRCLE_BRANCH
+echo $HEROKU_APP
+
+heroku git:remote -a $HEROKU_APP
